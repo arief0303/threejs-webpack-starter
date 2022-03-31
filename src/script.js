578c4fda18f8d2ddac30b44a50ca30a33a2811ae
arief0303 <arief0303@gmail.com>
2022-03-31 22:02:53 +0700

    init

--- src/script.js
+++ src/script.js
@@ -3,6 +3,9 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+// Texture Loader
+const textureLoader = THREE.TextureLoader
+
 // Debug
 const gui = new dat.GUI()
 
